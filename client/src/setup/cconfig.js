cadfd0c722a6b0b2a13f7ffc20425b0fbc76b5bb
SULEIMANE DUCURE <suleimane.ducure@aluno.ufop.edu.br>
2023-01-01 21:05:31 -0300

    api

--- client/src/setup/cconfig.js
+++ client/src/setup/cconfig.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 //https://varlson-api.onrender.com
 const Axios = axios.create({
-  baseURL: "http://localhost:9000/auth",
+  baseURL: "https://varlson-api.onrender.com",
 });
 Axios.defaults.headers.post["Content-Type"] = "application/json";
 export default Axios;
